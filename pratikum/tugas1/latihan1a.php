1c513ed7f0ac1242a42b0e4674c5d53421c0d759
ayu-203040058 <ayu.dinar078@gmail.com>
2021-03-04 13:59:25 +0700

    menyelesaikan tugas1

---
+++ pratikum/tugas1/latihan1a.php
@@ -0,0 +1,7 @@
+<!-- Nyimas Ayu Dinar 203040058 Pertemuan 1 (04 Maret 2021) -->
+<?php for ($x=1; $x<=3; $x++){
+    for ($z=1; $z<=3; $z++){
+        echo "Ini perulangan ke ($x,$z)<br>";
+    }
+}
+?>
