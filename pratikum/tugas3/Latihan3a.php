8829970db4545fa5a57eefbcbe1747fb9aacf6fb
ayu-203040058 <ayu.dinar078@gmail.com>
2021-03-17 20:56:34 +0700

    menyelesaikan tugas3

---
+++ pratikum/tugas3/Latihan3a.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+	Nyimas Ayu Dinar
+	203040058
+	github.com/Nyimas Ayu Dinar/pw2021_203040055
+	jumat 10-11
+*/
+
+    $array = ["ada", "abel", "men", "pung", "nilai"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        p {
+            border: 1px solid black;
+            width: 50%;
+            height: 40px;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <p>Array <?= $array[0];?>lah suatu vari<?= $array[1];?> yang dapat <?= $array[2];?>am<?= $array[3];?> banyak <?= $array[4];?>  </p>
+</body>
+</html>
